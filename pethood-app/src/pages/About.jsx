b42af2526843ffdfb1aaf6259966d4aa55b81630
RubenMoretti <rusfaider2001@gmail.com>
2026-01-22 17:21:44 +0100

    Aggiunto cartelle caratteri

--- pethood-app/src/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <div className="flex items-center justify-center h-72 rounded-lg shadow-lg bg-indigo-500 text-white text-3xl font-bold">
-      About — informazioni sull'app
-    </div>
-  )
-}
